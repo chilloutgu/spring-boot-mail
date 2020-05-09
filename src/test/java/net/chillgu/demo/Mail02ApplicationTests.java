d76338df2d26010c89e4759bdcb337f4063fd778
chillgu <bbonggu92@gmail.com>
2020-05-09 23:00:07 +0900

    first commit & set MailSender's properties

---
+++ src/test/java/net/chillgu/demo/Mail02ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chillgu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mail02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
